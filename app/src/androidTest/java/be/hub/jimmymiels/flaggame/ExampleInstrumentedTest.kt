b2c465f29e28ed87d289d34a0439a472670867d6
M-JM <jimmymiels@hotmail.com>
2020-01-11 00:08:08 +0100

    Succesfully tested Database in test env.

--- app/src/androidTest/java/be/hub/jimmymiels/flaggame/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/be/hub/jimmymiels/flaggame/ExampleInstrumentedTest.kt
@@ -1,24 +1,54 @@
 package be.hub.jimmymiels.flaggame
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.room.Room
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import be.hub.jimmymiels.flaggame.database.FinalScore
+import be.hub.jimmymiels.flaggame.database.ScoreDatabase
+import be.hub.jimmymiels.flaggame.database.ScoreDatabaseDao
+import org.junit.Assert.assertEquals
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
+import java.io.IOException
 
 /**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
+ * This is not meant to be a full set of tests. For simplicity, most of your samples do not
+ * include tests. However, when building the Room, it is helpful to make sure it works before
+ * adding the UI.
  */
+
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class ScoreDatabaseTest {
+
+    private lateinit var scoreDao: ScoreDatabaseDao
+    private lateinit var db: ScoreDatabase
+
+    @Before
+    fun createDb() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        // Using an in-memory database because the information stored here disappears when the
+        // process is killed.
+        db = Room.inMemoryDatabaseBuilder(context, ScoreDatabase::class.java)
+            // Allowing main thread queries, just for testing.
+            .allowMainThreadQueries()
+            .build()
+       scoreDao = db.scoreDatabaseDao
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
+    }
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("be.hub.jimmymiels.flaggame", appContext.packageName)
+    @Throws(Exception::class)
+    fun insertandgetScore() {
+        val score = FinalScore()
+       scoreDao.insert(score)
+        val finalScore1 = scoreDao.getFirstScore()
+        assertEquals(finalScore1?.finalScorevalue, -1)
     }
 }
