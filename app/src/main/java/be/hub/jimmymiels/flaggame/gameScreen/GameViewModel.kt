b61e301d7e5add5a8b4d9cc74b9bcce2833a94a2
M-JM <jimmymiels@hotmail.com>
2020-01-10 21:39:30 +0100

    Logic to bind mutable value to layout and return size of the List array ( JSON converted by Moshi ) called from API

--- app/src/main/java/be/hub/jimmymiels/flaggame/gameScreen/GameViewModel.kt
+++ app/src/main/java/be/hub/jimmymiels/flaggame/gameScreen/GameViewModel.kt
@@ -1,7 +1,35 @@
 package be.hub.jimmymiels.flaggame.gameScreen
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import be.hub.jimmymiels.flaggame.apiCountry.CountryApi
+import be.hub.jimmymiels.flaggame.apiCountry.CountryProperties
+import retrofit2.Call
+import retrofit2.Response
+import javax.security.auth.callback.Callback
 
-class GameViewModel : ViewModel(){
+class GameViewModel : ViewModel() {
 
+    private val _response = MutableLiveData<String>()
+    val response: LiveData<String>
+        get() = _response
+
+    init {
+        getCountries()
+    }
+
+    private fun getCountries() {
+        CountryApi.retrofitService.getProperties().enqueue(object : retrofit2.Callback<List<CountryProperties>> {
+            override fun onFailure(call: Call<List<CountryProperties>>, t: Throwable) {
+                _response.value = "Failure" + t.message
+            }
+
+            override fun onResponse(call: Call<List<CountryProperties>>, response: Response<List<CountryProperties>>) {
+                _response.value = "Succes: ${response.body()?.size} countries retrieved"
+            }
+
+        }
+        )
+    }
 }
