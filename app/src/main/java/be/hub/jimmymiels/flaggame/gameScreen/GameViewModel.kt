cb08738313da869296140d87914394079faf7576
M-JM <jimmymiels@hotmail.com>
2020-01-18 12:10:23 +0100

    Added game functions :
    - Get a random flag and corresponding values set as correct answer.
    - Get 3 other countries properties in a list , add correct answer to list , shuffle and display in radiobutton group.

--- app/src/main/java/be/hub/jimmymiels/flaggame/gameScreen/GameViewModel.kt
+++ app/src/main/java/be/hub/jimmymiels/flaggame/gameScreen/GameViewModel.kt
@@ -1,6 +1,5 @@
 package be.hub.jimmymiels.flaggame.gameScreen
 
-import android.widget.ImageView
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,9 +7,37 @@ import be.hub.jimmymiels.flaggame.apiCountry.CountryApi
 import be.hub.jimmymiels.flaggame.apiCountry.CountryProperties
 import kotlinx.coroutines.*
 import java.util.*
+import kotlin.collections.ArrayList
 
 
 class GameViewModel : ViewModel() {
+
+    private val _testsize = MutableLiveData<String>()
+    val testsize: LiveData<String>
+        get() = _testsize
+
+    private val _option1 = MutableLiveData<String>()
+    val option1: LiveData<String>
+        get() = _option1
+
+    private val _option2 = MutableLiveData<String>()
+    val option2: LiveData<String>
+        get() = _option2
+
+    private val _option3 = MutableLiveData<String>()
+    val option3: LiveData<String>
+        get() = _option3
+
+    private val _option4 = MutableLiveData<String>()
+    val option4: LiveData<String>
+        get() = _option4
+
+
+    var queries = mutableListOf<String>()
+
+    private val _randomAnswers  = MutableLiveData<ArrayList<String>>()
+    val randomAnswers: LiveData<ArrayList<String>>
+    get() =_randomAnswers
 
     private val _randomflag1 = MutableLiveData<String>()
     val randomflag1: LiveData<String>
@@ -20,17 +47,25 @@ class GameViewModel : ViewModel() {
     val response: LiveData<String>
         get() = _response
 
+    private val _correctAnswer = MutableLiveData<String>()
+    val correctAnswer: LiveData<String>
+        get() = _correctAnswer
+
     private val _randomflag = MutableLiveData<String>()
     val randomflag: LiveData<String>
         get() = _randomflag
 
     private val _country = MutableLiveData<CountryProperties>()
-    val country:LiveData<CountryProperties>
-    get() = _country
+    val country: LiveData<CountryProperties>
+        get() = _country
 
     private val _countries = MutableLiveData<List<CountryProperties>>()
-    val countries:LiveData<List<CountryProperties>>
-    get() = _countries
+    val countries: LiveData<List<CountryProperties>>
+        get() = _countries
+
+    private val _correctanswer = MutableLiveData<List<CountryProperties>>()
+    val correctanswer: LiveData<List<CountryProperties>>
+        get() = _correctanswer
 
     private var viewModelJob = Job()
     private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
@@ -38,40 +73,98 @@ class GameViewModel : ViewModel() {
     init {
         getCountries()
         getRandomCountry()
+
+
     }
 
     private fun getCountries() {
-       coroutineScope.launch {
-      var getPropertiesDeferred =  CountryApi.retrofitService.getProperties()
-       try {
-              var listResult = getPropertiesDeferred.await()
-           _response.value = "Success: ${listResult.size} Mars properties retrieved"
-           _country.value = listResult[0]
-           _countries.value = listResult
-            }
-              catch (e: Exception) {
-           _response.value = "Failure : ${e.message}"
-       }
-        }
-
-   }
-
-     fun getRandomCountry() {
         coroutineScope.launch {
-            var getPropertiesDeferred =  CountryApi.retrofitService.getProperties()
+            var getPropertiesDeferred = CountryApi.retrofitService.getProperties()
             try {
                 var listResult = getPropertiesDeferred.await()
-                var random = Random().nextInt(250)
-                _randomflag.value = listResult[random].capital
-                _randomflag1.value =listResult[random].imgSrcUrl
+                var random = Random().nextInt(249)
+                _response.value = "Success: ${listResult.size} Mars properties retrieved"
+                //_country.value = listResult[random]
+                _countries.value = listResult
 
-            }
-            catch (e: Exception) {
+            } catch (e: Exception) {
                 _response.value = "Failure : ${e.message}"
+            }
+        }
 
     }
 
+    fun getRandomCountry() {
+        coroutineScope.launch {
+            var getPropertiesDeferred = CountryApi.retrofitService.getProperties()
+
+
+            try {
+                var listResult = getPropertiesDeferred.await()
+                var random = Random().nextInt(249)
+                 _correctAnswer.value = listResult[random].name
+                _correctanswer.value = listOf(listResult[random])
+                 _randomflag1.value = listResult[random].imgSrcUrl
+                _country.value = listResult[random]
+
+
+                //_option1.value = _correctAnswer.value!!
+                // _option2.value = listResult[random].name
+                // _option3.value = listResult[random].name
+                // _option4.value = listResult[random].name
+                // _randomAnswers.value!![0] =_correctAnswer.value!!
+                // queries[1] =_correctAnswer.value!!
+                //  _testsize.value = _randomAnswers.value!!.size.toString()
+                // queries[2] =_correctAnswer.value!!
+                // queries[3] =_correctAnswer.value!!
+                //queries[1] = _countries.value!![random].name
+                //queries[2] = _countries.value!![random2].name
+                //queries[3] = _countries.value!![random3].name
+
+                //_testsize.value = _randomAnswers.value!!.size
+               // getRandomAnswers()
+
+               // queries.shuffle()
+
+                getRandomAnswers()
+
+            } catch (e: Exception) {
+                _response.value = "Failure : ${e.message}"
+
+            }
+
+
         }
+    }
+
+    fun getRandomAnswers(){
+
+    var chars  =_countries.value!!.shuffled().take(3).toMutableList()
+
+        if (chars.isNotEmpty())
+        {
+            chars.add(_country.value!!)
+        }
+chars.shuffle()
+
+       // var random = Random().nextInt(249)
+       // var random2 = Random().nextInt(249)
+       // var random3 = Random().nextInt(249)
+
+        _option1.value = chars[0].name
+        _option2.value = chars[1].name
+        _option3.value = chars[2].name
+        _option4.value = chars[3].name
+        _testsize.value = chars.size.toString()
+
+
+    }
+
+    fun Onbutton() {
+
+        getRandomCountry()
+        getRandomAnswers()
+
     }
 
     override fun onCleared() {
